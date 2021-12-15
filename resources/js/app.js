11625ae5a51d5f8e5e7d9694315d1452994bc2f7
md irfan <md.irfan7839@gmail.com>
2021-12-15 22:47:12 +0530

    add to cart

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,34 @@
-console.log('Hello from app js')
+import axios from 'axios';
+import session from 'express-session';
+import { Notyf } from 'notyf';
+let addToCart=document.querySelectorAll('.add-to-cart');
+let cartCounter=document.querySelector('#cartCounter')
+
+
+function updateCart(pizza){
+    axios.post('/update-cart',pizza).then(res =>{
+        
+      
+        cartCounter.innerText = res.data.totalQty
+    })
+}
+
+addToCart.forEach((btn)=>{
+    btn.addEventListener('click',(e)=>{
+        
+        let pizza=JSON.parse(btn.dataset.pizza)
+        updateCart(pizza)
+        
+        var notyf = new Notyf();
+        notyf.success({
+            message:'added to cart !',
+            duration: 1000,
+            progressBar:false,
+            position: {
+              x: 'right',
+              y: 'top',
+            },
+        })
+    
+})
+})
